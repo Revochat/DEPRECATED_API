0df018983f476543fd4231ce06feee5bd992ee1b
Luxchar <luccharlopeau@gmail.com>
2023-02-01 15:30:52 +0100

    necessary fix to remove critical information from friends data..

--- src/utils/functions/index.ts
+++ src/utils/functions/index.ts
@@ -1,5 +1,6 @@
 import {findUserbyToken, findServer, findUserbyID, findFriendbyUser, findChannelID, findChannelFriend, findUserBlocked, findUserInServer} from "./find"
 import {PERMISSIONS} from "./permissions"
+import { IUser } from '../../database/models/User';
 
 export class FUNCTIONS {
     static PERMISSIONS = {
@@ -28,14 +29,14 @@ export class FUNCTIONS {
     }
     
     static REMOVE_PRIVATE_INFO = (user: any) => {
-        delete user.password
-        delete user.token
-        delete user.friends_requests_received
-        delete user.friends_requests_sent
-        delete user.blocked
-        delete user.servers
-        delete user.channels
-        delete user.friends
+        user.password = undefined
+        user.token = undefined
+        user.friends_requests_received = undefined
+        user.friends_requests_sent = undefined
+        user.blocked = undefined
+        user.servers = undefined
+        user.channels = undefined
+        user.friends = undefined
         return user
     }
 }
