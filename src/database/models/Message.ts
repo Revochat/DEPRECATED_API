821f1ca7a3e065a9962c3e1f62f69282026c559c
ByLife <bylife@bylife.fr>
2023-04-05 15:14:22 +0200

    Reset

--- src/database/models/Message.ts
+++ src/database/models/Message.ts
@@ -1,10 +1,8 @@
 import mongoose, {Document, Schema} from "mongoose";
-import { IUser } from "./User";
-import { IUserSanitized } from "./UserSanitized";
 
 export interface IMessage { // This is the interface for the message in the database
     message_id: number;
-    author: IUserSanitized;
+    user_id: number;
     channel_id: number;
     message: string;
     created_at: string;
@@ -14,7 +12,7 @@ export interface IMessageModel extends IMessage, Document {}
 
 const MessageSchema = new Schema({
     message_id: { type: Number, required: true, unique: true, index: true },
-    author: { type: Object, required: true },
+    user_id: { type: Number, required: true, index: true },
     channel_id: { type: Number, required: true, index: true },
     message: { type: String, required: true },
     created_at: { type: String, required: true, default: Date.toLocaleString() },
